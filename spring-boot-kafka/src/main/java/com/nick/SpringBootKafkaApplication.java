b05e2846e69c84cda972e7785440d6d4593ca611
dreamcatchernick <bingu@quarkfinance.com>
2017-09-15 18:15:34 +0800

    project init

---
+++ spring-boot-kafka/src/main/java/com/nick/SpringBootKafkaApplication.java
@@ -0,0 +1,12 @@
+package com.nick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKafkaApplication.class, args);
+	}
+}
